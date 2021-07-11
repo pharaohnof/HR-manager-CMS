4bc01944706258e8afaab6f3c65936a8539e8f04
Matthew Pawlak <pharaohnof@gmail.com>
2021-07-11 14:06:44 -0400

    made connection to db and included .env

---
+++ app.js
@@ -0,0 +1,17 @@
+//add dependencies
+const db = require('mysql2')
+const inquirer = require('inquirer')
+
+require('dotenv').config()
+
+//connect to db
+
+const connection = db.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || "root",  // enter your db username here if not root
+    password: process.env.DB_PASSWORD || "", //enter your db password here
+    database: process.env.DB_NAME ||'hr_tracker',
+
+})
+console.log(`connected to ${connection.config.host}:${connection.config.database} at PORT ${connection.config.port}`)
